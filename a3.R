94d92f8c7dae9ca2de198b7705629d6ac7f0e44c
jdobladez <104499165+jdobladez@users.noreply.github.com>
2022-06-10 20:11:57 +0200

    Add files via upload

--- a3.R
+++ a3.R
@@ -26,6 +26,20 @@ combinations_2 <- combn(seq(20), 2)
 data_2008.result.lm <- train(result ~ V1 + V2, data = data_2008, method = "lm",trControl = ctrl)
 summary(data_2008.result.lm)
 
+variables <- list("V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9")
+sapply(data_2008, lm())
+error = c()
+for (i in colnames(x_2008)){
+  print("for model")
+  print(i)
+  model <- lm(paste("result ~", i), data = data_2008)
+  #error[y]<- model[["results"]][["RMSE"]]
+  print(summary(model))
+}
+
+print(mean((data_2008$result - pred)^2))
+
+variables <- list(V10, V11, V12, V13, V14, V15, V16, V17, V18, V19, V20)
 #Getting RMSE
 print(data_2008.result.lm[["results"]][["RMSE"]])
 
