508f7510010d7163011abc1b3072bf814d6e626a
jdobladez <104499165+jdobladez@users.noreply.github.com>
2022-06-10 14:02:30 +0200

    with the combination function

--- a3.R
+++ a3.R
@@ -2,3 +2,6 @@ t_2008 <- read.table('~/sc/subset2008.txt')
 t_2007 <- read.table('~/sc/subset2007.txt')
 matrix_2008 <- as.matrix(t_2008)
 matrix_2007 <- as.matrix(t_2007)
+y <- seq(20)
+combinations <- combn(y, 4)
+# combn --> function to generate all combinations of n elements
