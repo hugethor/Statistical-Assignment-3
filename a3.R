750501eac5b71a55f41b1ad01f79f87488d9445c
jdobladez <104499165+jdobladez@users.noreply.github.com>
2022-06-10 17:23:39 +0200

    Add files via upload

--- a3.R
+++ a3.R
@@ -1,34 +1,29 @@
+library(caret)
+
 # Loading the predictors
-x_2008 <- read.table('~/sc/subset2008.txt')
-x_2007 <- read.table('~/sc/subset2007.txt')
+x_2008 <- read.table('subset2008.txt')
+x_2007 <- read.table('subset2007.txt')
 
 # Loading the results
-y_2008 <- read.table('~/sc/TITER2008_centered.txt')
-y_2007 <- read.table('~/sc/TITER2007_centered.txt')
+y_2008 <- read.table('TITER2008_centered.txt')
+y_2007 <- read.table('TITER2007_centered.txt')
 y_2007 <- t(y_2007)
+y_2007 <- as.data.frame(y_2007)
 
 # Change colname of one column
 colnames(y_2008)[colnames(y_2008) == "V1"] <- "result"
 colnames(y_2007)[colnames(y_2007) == "V1"] <- "result"
 
-# Turn into a vector
-y_08 <- as.vector(y_2008)
-y_07 <- as.vector(y_2007)
-
 # Creating the matrices
-matrix_2008 <- as.matrix(x_2008)
-matrix_2007 <- as.matrix(x_2007)
-matrix_2008 <- cbind(matrix_2008, y_08)
-matrix_2007 <- cbind(matrix_2007, y_07)
-data2007 <- as.data.frame(matrix_2007)
-data2008 <- as.data.frame(matrix_2008)
-# Creating the sequence & combinations
-seq <- seq(20)
-combinations_4 <- combn(seq, 4)
-combinations_3 <- combn(seq, 3)
-combinations_2 <- combn(seq, 2)
+data_2008 <- cbind(x_2008, y_2008)
+data_2007 <- cbind(x_2007, y_2007)
 
-data2008.result.lm <- lm(result ~ V1 + V2, data = data2008)
-summary(data2008.result.lm)
+# Creating the sequence & combinations
+combinations_4 <- combn(seq(20), 4)
+combinations_3 <- combn(seq(20), 3)
+combinations_2 <- combn(seq(20), 2)
+
+data_2008.result.lm <- lm(result ~ V1 + V2, data = data_2008)
+summary(data_2008.result.lm)
 
 
